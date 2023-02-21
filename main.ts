4fb3dd95382bfb09b7eaf69a8d6bbda34bc92fc2
jayant550 <125988605+jayant550@users.noreply.github.com>
2023-02-21 19:26:47 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . . .
+        # # # # #
+        # # . # #
+        . # # . .
+        . . # . .
+        `)
+    basic.showString("iloveu")
+})
